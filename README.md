135d33b02f70926b014ae26a72df2502537b012c
Joel U <joelulahanna@gmail.com>
2019-10-16 23:23:33 -0500

    initial setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# Holo Monitor
+
+The Holo Monitor keep track of when a hosts enable or disable an app for hosting my hosting device is automatically registered/deregistered with this Holo centralized services
+
+## How To
+
+### NodeJS App
+- clone the repo to your local machine
+- run `nix-shell`
+- run `npm install`
+- Make sure you have installed node and are using a version of node that supports `experimental-modules`
+- Run this:
+> `node --experimental-modules src/index.mjs`
+
+- For running on a server:
+> `npm start`
---
+++ default.nix
@@ -0,0 +1,110 @@
+{ pkgs ? import ./pkgs.nix {} }:
+
+with pkgs;
+
+let
+  dnaConfig = dna: {
+    id = dna.name;
+    file = "${dna}/${dna.name}.dna.json";
+    hash = dnaHash dna;
+  };
+
+  instanceConfig = dna: {
+    agent = "holo-monitor-agent";
+    dna = dna.name;
+    id = dna.name;
+    storage = {
+      path = ".holochain/holo/storage/${dna.name}";
+      type = "file";
+    };
+  };
+
+  dnas = with dnaPackages; [
+    happ-store
+    holo-hosting-app
+  ];
+in
+
+{
+  holo-envoy = stdenv.mkDerivation rec {
+    name = "holo-envoy";
+    src = gitignoreSource ./.;
+
+    nativeBuildInputs = [
+      holochain-cli
+      holochain-conductor
+      nodejs-12_x
+    ];
+
+    preConfigure = ''
+      cp -r ${npmToNix { inherit src; }} node_modules
+      chmod -R +w node_modules
+      patchShebangs node_modules
+    '';
+
+    buildPhase = ''
+      npm run build
+    '';
+
+    installPhase = ''
+      mkdir $out
+      mv * $out
+    '';
+
+    fixupPhase = ''
+      patchShebangs $out
+    '';
+
+    checkPhase = ''
+      npm run test:unit
+    '';
+
+    doCheck = true;
+  };
+
+  holo-monitor-conductor-config = writeTOML {
+    bridges = [];
+    persistence_dir = ".holochain/holo";
+    agents = [{
+      id = "holo-monitor-agent";
+      name = "Holo Monitor";
+      keystore_file = "agent.key";
+      public_address = "HcSCiNEDE7zGesteidU3Teckx9D5oqu5q96G99qyMJgYsqgrHIK9w8wGAEcvqtr";
+    }];
+    dnas = map dnaConfig dnas;
+    instances = map instanceConfig dnas;
+    interfaces = [
+      {
+        driver = {
+          port = 8800;
+          type = "websocket";
+        };
+        id = "master-interface";
+        instances = map (dna: { id = dna.name; }) dnas;
+      }
+      {
+        admin = true;
+        id = "public-interface";
+        driver = {
+          port = 8080;
+          type = "http";
+        };
+      }
+    ];
+    logger = {
+      type = "debug";
+      rules.rules = [
+        {
+          color = "red";
+          exclude = false;
+          pattern = "^err/";
+        }
+        {
+          color = "white";
+          exclude = false;
+          pattern = "^debug/dna";
+        }
+      ];
+    };
+  };
+}
